3d769426a945af4b44e4e9f4d6776b154babd528
GuilhermeProcopio <guilhermecprocopio@gmail.com>
2024-06-13 21:37:39 -0300

    feat: adding vpc

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 provider "aws" {
-  # Configuration options
+    region = "us-east-1"    
 }
 
 data "aws_ami" "ubuntu" {
@@ -33,5 +33,28 @@ resource "aws_instance" "web" {
 
   tags = {
     Name = "ec2-micro-instance"
+  }
+}
+
+output "private_ip" {
+ value = aws_instance.web.private_ip 
+}
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "vpcs-basic"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
   }
 }
